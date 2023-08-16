4a0775019077157d4183e2ee5bc1556c57d9e9c7
Nana Kwame <105567071+chiro999@users.noreply.github.com>
2023-08-16 04:52:12 +0000

    Add files via upload

---
+++ 128-bit(SSE).asm
@@ -0,0 +1,10 @@
+section .data
+    result dd 0      ; Store the result in a variable
+
+section .text
+    global _start
+
+_start:
+    vmovd xmm0, dword 7        ; Load value 7 into xmm0(in SSE(128-bit mode) more special registers known as xmm registers were introduced) register
+    vpaddd xmm0, xmm0, dword 3 ; Add value 3 to xmm0 register
+    vmovd [result], xmm0       ; Store the result in the 'result' variable
